1a611c061c5af80952b1e7c1b48c80c1077c8df8
DESKTOP-H0B2ILU\wadde <twaddell@edu-reality.com>
2024-10-24 10:20:24 -0700

    New structure

--- Collets/ColletSizingGuide.m
+++
@@ -1,9 +0,0 @@
-%%This Script is Used to Calculate the Internal Dimensions of the Collet%%
-
-compression_ratio=pi*d_vial/(pi*8);
-mat=[0;0];
-d_lower=d_vial-compression_ratio;
-d_upper=d_vial+compression_ratio;
-mat(1,1)=[d_lower];
-mat(2,1)=[d_upper];
-mat
